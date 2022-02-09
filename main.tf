87e7d3435fb831257dc51a8adc4237aa0e098286
mysura123 <90843033+mysura123@users.noreply.github.com>
2022-02-09 22:06:52 +0530

    Revert "terraform modules"

--- main.tf
+++ main.tf
@@ -3,7 +3,12 @@ provider "aws" {
   
 }
 
-
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable instance_type {}
+# variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -12,13 +17,39 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-module "myapp-subnet" {
-  source = "./modules/subnet"
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
+resource "aws_subnet" "myapp-subnet-1" {
   vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+      Name = "${var.env_prefix}-myapp-igw"
+    }
+  
+}
+
+resource "aws_route_table" "myapp_route" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
     
+  } 
+  tags = {
+      Name = "${var.env_prefix}-myapp-rtb"
+  }
+}
+
+resource "aws_route_table_association" "myapp-ass-rtb" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp_route.id
 }
 
 resource "aws_security_group" "myapp-sg" {
@@ -65,7 +96,9 @@ data "aws_ami" "latest-amazon-linux" {
   }
 }
 
-
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux.id
+}
 
 #resource "aws_key_pair" "ssh-key" {
  # key_name ="server-key"
@@ -77,7 +110,7 @@ resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux.id
   instance_type = var.instance_type
 
-  subnet_id = module.myapp-subnet.subnet.id
+  subnet_id = aws_subnet.myapp-subnet-1.id
   vpc_security_group_ids = [ aws_security_group.myapp-sg.id ]
   availability_zone = var.avail_zone
 
@@ -92,3 +125,6 @@ user_data = file("entry-script.sh")
   }
 }
 
+output "aws_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable instance_type {} 
-variable vpc_id {}
-# variable public_key_location {}
